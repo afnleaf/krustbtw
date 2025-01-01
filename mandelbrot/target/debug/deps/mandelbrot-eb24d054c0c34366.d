2652f4d532ab2ad25c84ee58f36ccc6becaabd2b
afnleaf <afnleafow@gmail.com>
2025-01-01 15:33:31 -0500

    mandel

---
+++ mandelbrot/target/debug/deps/mandelbrot-eb24d054c0c34366.d
@@ -0,0 +1,5 @@
+/Users/home/Code/krustbtw/mandelbrot/target/debug/deps/mandelbrot-eb24d054c0c34366: src/main.rs
+
+/Users/home/Code/krustbtw/mandelbrot/target/debug/deps/mandelbrot-eb24d054c0c34366.d: src/main.rs
+
+src/main.rs:
---
+++ mandelbrot/target/debug/deps/num_rational-8a5c0a5d6867b1a1.d
@@ -0,0 +1,8 @@
+/Users/home/Code/krustbtw/mandelbrot/target/debug/deps/libnum_rational-8a5c0a5d6867b1a1.rmeta: /Users/home/.cargo/registry/src/index.crates.io-6f17d22bba15001f/num-rational-0.4.2/src/lib.rs /Users/home/.cargo/registry/src/index.crates.io-6f17d22bba15001f/num-rational-0.4.2/src/pow.rs
+
+/Users/home/Code/krustbtw/mandelbrot/target/debug/deps/libnum_rational-8a5c0a5d6867b1a1.rlib: /Users/home/.cargo/registry/src/index.crates.io-6f17d22bba15001f/num-rational-0.4.2/src/lib.rs /Users/home/.cargo/registry/src/index.crates.io-6f17d22bba15001f/num-rational-0.4.2/src/pow.rs
+
+/Users/home/Code/krustbtw/mandelbrot/target/debug/deps/num_rational-8a5c0a5d6867b1a1.d: /Users/home/.cargo/registry/src/index.crates.io-6f17d22bba15001f/num-rational-0.4.2/src/lib.rs /Users/home/.cargo/registry/src/index.crates.io-6f17d22bba15001f/num-rational-0.4.2/src/pow.rs
+
+/Users/home/.cargo/registry/src/index.crates.io-6f17d22bba15001f/num-rational-0.4.2/src/lib.rs:
+/Users/home/.cargo/registry/src/index.crates.io-6f17d22bba15001f/num-rational-0.4.2/src/pow.rs:
